8b35bb814c3f0d27e54d4671bc0ca24e5b9e3f99
JoryAlharbi <144896456+JoryAlharbi@users.noreply.github.com>
2023-10-01 09:35:31 +0300

    Add files via upload

---
+++ src/Node.java
@@ -0,0 +1,16 @@
+
+package ds.project;
+
+public class Node <T>{
+    
+    public T Data ;
+    public Node next ; 
+
+    public Node(T Data) {
+        this.Data = Data;
+        next= null ;
+    }
+    
+    
+    
+}
