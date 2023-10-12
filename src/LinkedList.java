435aa3045e624bfcbb91dde648ed1a8da02c0fe9
JoryAlharbi <144896456+JoryAlharbi@users.noreply.github.com>
2023-10-12 13:10:18 +0300

    Update LinkedList.java

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -82,24 +82,7 @@ public class LinkedList<T> {
             
     
 }
-ublic class LinkedList {
-     
-    private Node <T> head ; 
-    private Node <T> current ; 
-    
-    public LinkedList() {
-        head = null ; 
-    }
-    
-    // the basic methods 
-    
-    public boolean isEmpty(){
-        if (head==null)
-            return true ; 
-        else
-            return false;
-    }
-   
+
     //Not needed rn 
     
    /* public void insertFirst(int data){
@@ -111,51 +94,4 @@ ublic class LinkedList {
         
     } */
     
-    public boolean Full(){
-        return false ; //linked list is never full
-    }
-    
-    public boolean Last(){
-        if (current.next==null)
-            return true ; 
-        else
-            return false ;
-        
-    }
-    
-    public void FindNext(){
-        current=current.next ;
-    }
-    
-    public void Update(T d){
-    current.Data=d ;
-    }
-    
-    public T Retrive(){
-        return current.Data;
-    }
-    
-    public void Insert(T data){
-        Node <T> temp=new Node <T> (data); 
-        
-        if (head==null){
-            head=temp ; 
-            current=head ;
-        }
-        
-        else {
-            temp.next= current.next ;
-            current.next=temp ; 
-            current= temp ;
-        }
-        
-    }
-    
-    public void Remove(){
-        
-    }
-            
-            
-            
-            
-}
+  
