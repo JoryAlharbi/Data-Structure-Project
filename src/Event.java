f56b1498e7ad2897c0e512a04f1f92ae0d1fd608
RawanSaud23 <129692073+RawanSaud23@users.noreply.github.com>
2023-10-16 16:28:20 +0300

    Update Event.java

--- src/Event.java
+++ src/Event.java
@@ -1,3 +1,27 @@
+package com.mycompany.ds;
+
 public class Event {
+    private String title;
+    private String date;
+    private String time;
+    private String location;
+    private Contacts<T> contact;
+
+    public Event(String title, String date, String time, String location, Contacts contact) {
+        this.title = title;
+        this.date = date;
+        this.time = time;
+        this.location = location;
+        this.contact = contact;
+    }
     
+    //seeters ang getters:
+    
+    /* ??????????Chatgpt:
+    public boolean equals(Object o){
+        
+        return true;
+    }
+    */
+   
 }
